d724ce43ef00740f7dd86a1fb47069bba154a2b8
BlackSpider <rrgi89@hotmail.com>
2020-02-01 15:26:23 -0300

    fragment de recetas agregado
    layout de medicamento

---
+++ app/src/main/java/com/blackspider/recetapp/fragments/RecetaFragment.kt
@@ -0,0 +1,26 @@
+package com.blackspider.recetapp.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.blackspider.recetapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class RecetaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_receta, container, false)
+    }
+
+
+}
