2db88e60cadfe7b52ce542c03dbe3726a4bf1e40
RickSpider <rrgi89@hotmail.com>
2020-02-08 15:20:07 -0300

    nuevo fragment de carga de recetas mejoras visuales

---
+++ app/src/main/java/com/blackspider/recetapp/fragments/CargarRecetaFragment.kt
@@ -0,0 +1,17 @@
+package com.blackspider.recetapp.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.blackspider.recetapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CargarRecetaFragment : Fragment(R.layout.fragment_cargar_receta) {
+
+}
